a11cb72936000dba5900b6842e9208a178b8d559
Nathana2022 <nathanaeldon495@gmail.com>
2024-03-29 18:26:20 +0100

    passport strategy local management

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const bcrypt = require('bcrypt')
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 const usersRouter = require("./routes/users");
 const ordinateursRouter = require("./routes/ordinateurs");
+const prisma = require("./db/prisma");
+
 
 dotenv.config();
-const PORT = process.env.PORT;
+
+//configuration de Strategie Locale
+const confingPassport = new LocalStrategy({usernameField : 'email', passwordField:'password'}, async (email, password, done)=>{
+  const user = await prisma.user.findFirst({where:{email}})
+  if(!user){return done(null, false, {message:'Cet utilisateur ne se trouve pas dans notre système'})}
+   bcrypt.compare(password, user.password, (error, ismatch)=>{
+    if (error){return done(error)}
+   })
+   if(!ismatch){return done(null, false, {message: 'le mot de passe est incorrect'})}
+   return done(null, user)
+});
+
+//configuration de passport
+passport.use(confingPassport);
+passport.serializeUser((user, done) => {
+  done(null, user.id)
+});
+passport.deserializeUser((user, done) => {
+  done(null, user)
+});
+
+const PORT = process.env.PORT || 3002;
 const server = express();
 server.use(express.json());
 
